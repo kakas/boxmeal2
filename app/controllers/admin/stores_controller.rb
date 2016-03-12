5df0e3969a9e5e3a29ff54ce8af5898194a5384a
kakas <shrimptrain@gmail.com>
2016-03-12 16:20:28 +0800

    function: 新增店家

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,2 +1,23 @@
 class Admin::StoresController < ApplicationController
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+
+    if @store.save
+      redirect_to admin_stores_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :phone, :address)
+  end
+
 end
