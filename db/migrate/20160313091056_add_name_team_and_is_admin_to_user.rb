4dcd01bc680510cb23e242d374a8de40f745e21b
kakas <shrimptrain@gmail.com>
2016-03-13 17:13:08 +0800

    add name team and is_admin to user

---
+++ db/migrate/20160313091056_add_name_team_and_is_admin_to_user.rb
@@ -0,0 +1,7 @@
+class AddNameTeamAndIsAdminToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :team, :string
+    add_column :users, :is_admin, :boolean, default: false
+  end
+end
