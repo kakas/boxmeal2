8a39501ad55430fa5ffafbbee2d14609d136717b
kakas <shrimptrain@gmail.com>
2016-03-13 11:45:38 +0800

    add the columns provider (string) and uid (string) to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313034326) do
+ActiveRecord::Schema.define(version: 20160313034509) do
 
   create_table "products", force: :cascade do |t|
     t.integer  "store_id"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20160313034326) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
