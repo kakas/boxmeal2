35a1af4c07236c07b960c003de6c3d6008c477ee
Will <afil@icloud.com>
2016-03-13 20:15:36 +0800

    admin can add multiple products

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,7 @@
 class Store < ActiveRecord::Base
 
-  has_many :products, dependent: :destroy
+  #has_many :products, dependent: :destroy
+  has_many :products
+  accepts_nested_attributes_for :products, reject_if: :all_blank, allow_destroy: true
 
 end
