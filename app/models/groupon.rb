4a4c5af9939fd5b429e8a3ce96664c89cf7bac2e
kakas <shrimptrain@gmail.com>
2016-03-14 16:30:36 +0800

    建立order 和 order_item 關聯

--- app/models/groupon.rb
+++ app/models/groupon.rb
@@ -3,6 +3,8 @@ class Groupon < ActiveRecord::Base
   has_many :groupon_hosts
   has_many :hosts, through: :groupon_hosts, source: :user
   belongs_to :store
+  has_many :orders
+  has_many :order_items
 
   before_create :generate_token
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :groupon
+  has_many :order_items
+  has_many :item, through: :order_items, source: :product
+
 end
