1b4a5d88e5f0d7e07d9c665cc241f0361c1ded33
kakas <shrimptrain@gmail.com>
2016-03-14 15:50:23 +0800

    建立order model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314061606) do
+ActiveRecord::Schema.define(version: 20160314074642) do
 
   create_table "groupon_hosts", force: :cascade do |t|
     t.integer  "user_id"
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 20160314061606) do
   create_table "hosts", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "groupon_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "groupon_id"
+    t.integer  "user_id"
+    t.integer  "price"
+    t.boolean  "is_paid"
+    t.string   "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
