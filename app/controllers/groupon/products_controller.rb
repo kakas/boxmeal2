efc082d4dd2e9a294bf792e8f4390676962fb42c
kakas <shrimptrain@gmail.com>
2016-03-15 10:53:02 +0800

    訂購時排除重複商品，並跳出警告

--- app/controllers/groupon/products_controller.rb
+++ app/controllers/groupon/products_controller.rb
@@ -9,9 +9,13 @@ class Groupon::ProductsController < ApplicationController
     @product = Product.find(params[:id])
 
     @my_order = my_order(@groupon, current_user)
-    @my_order.add_product_to_order(@product)
+    if @my_order.items.include?(@product)
+      flash[:warning] = "#{@product.title} 已經下訂過囉～請直接修改數量即可。"
+    else
+      @my_order.add_product_to_order(@product)
+      flash[:success] = "訂購 #{@product.title} 成功。"
+    end
 
-    flash[:success] = "訂購 #{@product.title} 成功。"
     redirect_to order_path(@my_order.token)
   end
 
