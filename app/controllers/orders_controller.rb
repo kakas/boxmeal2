c14b1431b48a579a039785942e3a45701e0e6b10
kakas <shrimptrain@gmail.com>
2016-03-15 14:26:40 +0800

    function: 我的訂單列表

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,10 @@
 class OrdersController < ApplicationController
 
+  before_action :authenticate_user!
+
+  def index
+    @orders = current_user.orders.includes(order_items: :product, groupon: :store)
+  end
 
   def show
     @order = Order.includes(order_items: :product).find_by_token(params[:id])
