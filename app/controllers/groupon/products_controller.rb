0c0682c3c8411bdeb86dab0fba305e36b265caae
kakas <shrimptrain@gmail.com>
2016-03-15 10:38:40 +0800

    訂購商品後跳至訂單頁面

--- app/controllers/groupon/products_controller.rb
+++ app/controllers/groupon/products_controller.rb
@@ -12,7 +12,7 @@ class Groupon::ProductsController < ApplicationController
     @my_order.add_product_to_order(@product)
 
     flash[:success] = "訂購 #{@product.title} 成功。"
-    redirect_to groupon_products_path(@groupon.token)
+    redirect_to order_path(@my_order.token)
   end
 
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,8 @@
 class OrdersController < ApplicationController
+
+
+  def show
+    @order = Order.includes(order_items: :product).find_by_token(params[:id])
+  end
+
 end
