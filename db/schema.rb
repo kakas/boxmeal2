3ef546e8df51fbdecb2d23b9d483825e4080aaed
kakas <shrimptrain@gmail.com>
2016-03-16 16:37:06 +0800

    add is_drink to store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316045703) do
+ActiveRecord::Schema.define(version: 20160316083624) do
 
   create_table "groupon_hosts", force: :cascade do |t|
     t.integer  "user_id"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20160316045703) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "is_drink"
   end
 
   create_table "users", force: :cascade do |t|
