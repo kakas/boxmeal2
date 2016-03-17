e8cf5aec7b9340cae7ec1ad188da026b4165ba0f
kakas <shrimptrain@gmail.com>
2016-03-17 11:17:33 +0800

    刪除之前對devise做的設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,6 @@ class User < ActiveRecord::Base
   has_many :groupons, through: :groupon_hosts
   has_many :orders, dependent: :destroy
 
-  validates :email, presence: true
-  validates :name, presence: true
-  validates :team, presence: true
-
   def to_admin!
     self.update_columns(is_admin: true)
   end
