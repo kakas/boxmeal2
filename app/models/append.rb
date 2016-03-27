06bc879a5194b96ba5305c4a5c3473d3a761d820
kakas <shrimptrain@gmail.com>
2016-03-27 21:38:17 +0800

    建立訂單物品與附加物（珍珠、椰果、貢丸之類的）的關聯

--- app/models/append.rb
+++ app/models/append.rb
@@ -1,2 +1,6 @@
 class Append < ActiveRecord::Base
+
+  belongs_to :order_item
+  belongs_to :product_option
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,5 +3,7 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
   belongs_to :groupon
+  has_many :appends
+  has_many :product_options, through: :appends
 
 end
