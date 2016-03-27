ffc1d6e62f0581e2b3151857b3f182aed7cdbe86
kakas <shrimptrain@gmail.com>
2016-03-28 00:55:03 +0800

    將可修改及不可修改的訂單檢視頁面分開，增加程式可讀性

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -4,4 +4,10 @@ module OrdersHelper
     !order.is_paid? && !order.groupon.times_up?
   end
 
+  def render_delete_order_path(order, order_item)
+    link_to order_order_item_path(@order, order_item), method: :delete, data: { confirm: "確定刪除#{order_item.product.title}"  } do
+      content_tag :i, " ", class: "fa fa-trash fa-2x"
+    end
+  end
+
 end
